8d55fbd225022c47caab281ca292535ff21783df
Tomas <tvera@klabs.cl>
2014-04-14 23:55:38 -0300

    fix )

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ var container = require('vertx/container');
 vertx.createHttpServer().requestHandler(function(req) {
   req.response.headers['Content-Type'] = "text/plain";
   req.response.end("Hola mundo desde vertx");
-}).listen(parseInt(container.env.get('PORT'));
+}).listen(parseInt(container.env.get('PORT')));
