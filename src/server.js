35a044af16e045bf2b91fa8dd4c0bcadbd7b8515
Tomas <tvera@klabs.cl>
2014-04-14 23:51:41 -0300

    Modificacion de la forma en la cual se ingresa el puerto

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 var vertx = require('vertx')
 
+var container = require('vertx/container');
+
 vertx.createHttpServer().requestHandler(function(req) {
   req.response.headers['Content-Type'] = "text/plain";
   req.response.end("Hola mundo desde vertx");
-}).listen(8080);
+}).listen(parseInt(container.env.get('PORT'));
